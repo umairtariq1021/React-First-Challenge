dee9239c0cc83775270922703948c44aea0762ce
Fahad <jtfahadhassan@gmail.com>
2023-06-18 13:01:26 +0500

    day greetings messages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
 import { getActiveElement } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 
-const FirstName = "Umair"; 
-const LastName = "Tariq";
-const date = new Date().toLocaleDateString();
-const currentTime = new Date().toLocaleTimeString();
+// const FirstName = "Umair"; 
+// const LastName = "Tariq";
+// const date = new Date().toLocaleDateString();
+const currentTime = new Date().getHours();
+
+let greetingMessage = "";
+const greetingSyle = {}; 
+
+if(currentTime >= 1 && currentTime < 12 ){
+  greetingMessage = "Good Morning";
+  greetingSyle.color = 'green';
+}else if(currentTime >= 12 && currentTime < 19){
+  greetingMessage = "Good Afternoon";
+  greetingSyle.color = 'orange';
+}else{
+  greetingMessage = "Good Night";
+  greetingSyle.color = 'black';
+}
+// currentTime = currentTime
 
 ReactDOM.render(
   <>
-    <h1>{`Hello, My Nam is ${FirstName} ${LastName} `}</h1>
-
+    <div>
+      <h1>Hello, <span style= {greetingSyle}>{greetingMessage} </span></h1>
+    </div>
+{/* 
     <p>{`Today's Date is ${date}`}</p>
-    <p>{`Current Time is ${currentTime}`}</p>
+    <p>{`Current Time is ${currentTime}`}</p> */}
     {/* <p>{`Top ${Numb} ${platForm} Web series`}</p> */}
     {/* <ol>
       <li>Mirzapur</li>
