5f144b30959848ee59ea04a2a35f356a116106da
Fahad <jtfahadhassan@gmail.com>
2023-06-18 11:28:27 +0500

    second challenge

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+const FirstName = "Umair"; 
+const LastName = "Tariq";
+const date = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
+
 ReactDOM.render(
   <>
+    <h1>{`Hello, My Nam is ${FirstName} ${LastName} `}</h1>
 
-    <h1>First Challenge</h1>
-    <p>Top 5 Amazon Web series</p>
-    <ol>
+    <p>{`Today's Date is ${date}`}</p>
+    <p>{`Current Time is ${currentTime}`}</p>
+    {/* <p>{`Top ${Numb} ${platForm} Web series`}</p> */}
+    {/* <ol>
       <li>Mirzapur</li>
       <li>Breath</li>
       <li>Panchayat</li>
       <li>Farzi</li>
       <li>Hostel Daze</li>
-    </ol>
+    </ol> */}
     </>
   ,
   
