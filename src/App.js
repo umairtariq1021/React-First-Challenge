886b1b14e6eb71426c36e19759036ac03c8099d9
Fahad <jtfahadhassan@gmail.com>
2023-06-25 13:21:08 +0500

    props intro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import Greetings from './Greetings';
+// import Greetings from './Greetings';
 import {add,sub} from './Calculator';
+import NamesList from './NamesList';
 
 
 function App()
 {
   return (
     <>
-    <ul>
+    <NamesList   
+      Name= "Umair"
+     />
+     <NamesList   
+      Name= "Zubair"
+     />
+    {/* <ul>
       <li>Sum of two numbers is {add(4,5)}</li>
       <li>Sub of two numbers is {sub(6,5)}</li>
-    </ul>
+    </ul> */}
   </>
   );
   
---
+++ src/NamesList.jsx
@@ -0,0 +1,14 @@
+function NameList(props)
+{
+    return(
+        <>
+            <ul>
+                <li>
+                    My Name is {props.Name}
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default NameList;
