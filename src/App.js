5321620b0cd24e2d99072cd31214896fcf7abfba
Fahad <jtfahadhassan@gmail.com>
2023-06-25 12:48:58 +0500

    calculatior with import export

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Greetings from './Greetings';
+import {add,sub} from './Calculator';
 
 
 function App()
 {
   return (
     <>
-    <div>
-    <Greetings />
-    </div>
+    <ul>
+      <li>Sum of two numbers is {add(4,5)}</li>
+      <li>Sub of two numbers is {sub(6,5)}</li>
+    </ul>
   </>
   );
   
